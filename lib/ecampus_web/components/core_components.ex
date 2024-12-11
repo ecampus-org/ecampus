bae3e45f818a7a2e10ba19411b1fc009c5aaa635
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2024-12-11 15:55:20 +0300

    feat: daisyui dialog added

--- lib/ecampus_web/components/core_components.ex
+++ lib/ecampus_web/components/core_components.ex
@@ -43,49 +43,28 @@ defmodule EcampusWeb.CoreComponents do
 
   def modal(assigns) do
     ~H"""
-    <div
+    <dialog
       id={@id}
+      class="modal modal-bottom sm:modal-middle"
       phx-mounted={@show && show_modal(@id)}
       phx-remove={hide_modal(@id)}
+      phx-hook="Dialog"
       data-cancel={JS.exec(@on_cancel, "phx-remove")}
-      class="relative z-50 hidden"
     >
-      <div id={"#{@id}-bg"} class="bg-zinc-50/90 fixed inset-0 transition-opacity" aria-hidden="true" />
-      <div
-        class="fixed inset-0 overflow-y-auto"
-        aria-labelledby={"#{@id}-title"}
-        aria-describedby={"#{@id}-description"}
-        role="dialog"
-        aria-modal="true"
-        tabindex="0"
-      >
-        <div class="flex min-h-full items-center justify-center">
-          <div class="w-full max-w-3xl p-4 sm:p-6 lg:py-8">
-            <.focus_wrap
-              id={"#{@id}-container"}
-              phx-window-keydown={JS.exec("data-cancel", to: "##{@id}")}
-              phx-key="escape"
-              phx-click-away={JS.exec("data-cancel", to: "##{@id}")}
-              class="shadow-zinc-700/10 ring-zinc-700/10 relative hidden rounded-2xl bg-white p-14 shadow-lg ring-1 transition"
-            >
-              <div class="absolute top-6 right-5">
-                <button
-                  phx-click={JS.exec("data-cancel", to: "##{@id}")}
-                  type="button"
-                  class="-m-3 flex-none p-3 opacity-20 hover:opacity-40"
-                  aria-label={gettext("close")}
-                >
-                  <.icon name="hero-x-mark-solid" class="h-5 w-5" />
-                </button>
-              </div>
-              <div id={"#{@id}-content"}>
-                {render_slot(@inner_block)}
-              </div>
-            </.focus_wrap>
-          </div>
+      <div class="modal-box">
+        <h3 class="text-lg font-bold" id={"#{@id}-title"}>
+          {gettext("Modal Title")}
+        </h3>
+        <div id={"#{@id}-content"} class="py-4">
+          {render_slot(@inner_block)}
+        </div>
+        <div class="modal-action">
+          <button phx-click={JS.exec("data-cancel", to: "##{@id}")} type="button" class="btn">
+            {gettext("Close")}
+          </button>
         </div>
       </div>
-    </div>
+    </dialog>
     """
   end
 
@@ -621,27 +600,12 @@ defmodule EcampusWeb.CoreComponents do
 
   def show_modal(js \\ %JS{}, id) when is_binary(id) do
     js
-    |> JS.show(to: "##{id}")
-    |> JS.show(
-      to: "##{id}-bg",
-      time: 300,
-      transition: {"transition-all transform ease-out duration-300", "opacity-0", "opacity-100"}
-    )
-    |> show("##{id}-container")
-    |> JS.add_class("overflow-hidden", to: "body")
-    |> JS.focus_first(to: "##{id}-content")
+    |> JS.dispatch("dialog-show", to: "##{id}")
   end
 
   def hide_modal(js \\ %JS{}, id) do
     js
-    |> JS.hide(
-      to: "##{id}-bg",
-      transition: {"transition-all transform ease-in duration-200", "opacity-100", "opacity-0"}
-    )
-    |> hide("##{id}-container")
-    |> JS.hide(to: "##{id}", transition: {"block", "block", "hidden"})
-    |> JS.remove_class("overflow-hidden", to: "body")
-    |> JS.pop_focus()
+    |> JS.dispatch("dialog-hide", to: "##{id}")
   end
 
   @doc """
